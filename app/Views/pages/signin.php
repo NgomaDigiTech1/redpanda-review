aef135f462a6ce0fcf7cb38dfd24dd86a8b469e3
MrdKongolo <medardbaraka09@gmail.com>
2023-09-05 18:03:28 +0200

    updating registration emails

--- app/Views/pages/signin.php
+++ app/Views/pages/signin.php
@@ -35,6 +35,12 @@
                                  style="height: 60px; width: 120px;"
                             >
                             <h4 class="mb-3 f-w-400">Sign In</h4>
+                            <?php if(session()->getFlashdata('error')):?>
+                                <div class="alert alert-danger"><?=session()->getFlashdata('error');?></div>
+                            <?php endif;?>
+                            <?php if(session()->getFlashdata('success')):?>
+                                <div class="alert alert-danger"><?=session()->getFlashdata('success');?></div>
+                            <?php endif;?>
                             <div class="form-group mb-3">
                                 <label class="floating-label" for="Email">Email address</label>
                                 <input type="text" class="form-control" id="Email" name="user_email" placeholder=""
@@ -50,11 +56,11 @@
                             </div>
                             <div class="custom-control custom-checkbox text-left mb-4 mt-2">
                                 <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                <label class="custom-control-label" for="customCheck1">Save credentials.</label>
                             </div>
                             <button class="btn btn-block btn-primary mb-4" role="submit">Signin</button>
                             <p class="mb-2 text-muted">Forgot password? <a href="<?= base_url('reset');?>" class="f-w-400">Reset</a></p>
                             <p class="mb-0 text-muted">Don’t have an account? <a href="<?= base_url('signup');?>" class="f-w-400">Sign up</a></p>
+                            <p class="mb-0 text-muted">Go <a href="<?= base_url();?>" class="f-w-400">Home</a></p>
                         </div>
                     </div>
                 </div>
