ad3e38e693fd6ac62961ac2cf60ddca5cb6fde14
MrdKongolo <medardbaraka09@gmail.com>
2023-09-26 09:45:11 +0200

    config

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:80/redpanda-review/';
+	public $baseURL = '';
 
 	/**
 	 * --------------------------------------------------------------------------
